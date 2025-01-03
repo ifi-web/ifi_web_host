1e2dcef9247bad2d0fb7be03ce8c1c5ffa5b945a
ifi-web <ixiflower.freakless2024@gmail.com>
2025-01-03 15:33:03 +0330

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,9 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: true, // This allows the server to be accessible over the network
-    port: 5173, // You can choose any port number you want
-    open: true, // Optional: Automatically open the app in the browser
-  },
+  base: "/ifi_web_host/",
 });
